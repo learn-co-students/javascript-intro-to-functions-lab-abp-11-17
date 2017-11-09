f3c28c1beb39dcb690c33a7ffb10b2f9d6d2766f
Danny Forrest <dannyforrest@gmail.com>
2017-11-09 18:25:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,10 @@ function logShout(string) {
 }
 
 function sayHiToGrandma(string) {
-
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+    else if (string === string.toUpperCase())
+    return "YES INDEED!"
+  }
+  
 }
