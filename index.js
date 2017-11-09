9f4cd788ce0a6a9d0c73dbd2073c122a1f094320
Danny Forrest <dannyforrest@gmail.com>
2017-11-09 18:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logShout(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
 }  else if (string === string.toLowerCase()) {
     return "I can't hear you!"
