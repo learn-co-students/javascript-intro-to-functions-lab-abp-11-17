cc12138b427dc6b56b4baf6163907586cfe8126b
Danny Forrest <dannyforrest@gmail.com>
2017-11-09 17:48:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function shout(strin) {
+function shout(string) {
   string = string.toUpperCase()
+  return string
+}
+
+function whisper(string) {
+  string = string.toLowerCase()
   return string
 }
