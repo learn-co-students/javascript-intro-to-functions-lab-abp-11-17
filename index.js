7094aff85de1cf830c4cde274dd2ccc819f8fca9
Cristobal Heiss <ceheiss@uc.cl>
2017-11-10 03:46:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase(););
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase(););
+}
+
+
+shout("hello");
