1927b6eb7e4f2e2f3f9b111292532cb0daf7c7f1
icnpdesign <inp@bu.edu>
2017-11-10 01:21:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function shout(word) {
+  return word.toUpperCase();
+}
+
+function whisper(word) {
+  return word.toLowerCase();
+}
+
+function logShout (word) {
+  console.log(word.toUpperCase());
+}
+
+function logWhisper(word) {
+  console.log(word.toLowerCase());
+}
+
+function sayHiToGrandma (phrase) {
+  if (phrase.toUpperCase() === phrase) {
+    return "YES INDEED!";
+  } else if ("I love you, Grandma." === phrase) {
+    return "I love you, too." }
+    else { return "I can't hear you!"}
+}
