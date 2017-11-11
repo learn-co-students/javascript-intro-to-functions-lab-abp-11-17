da4cde3b6e95a0d05b0afaf5487dd159dc39e651
Victor Santana <mnmlbklyn@gmail.com>
2017-11-11 01:41:05 +0000

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-return 'hello'.toLowerCase()
 
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+
+
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
