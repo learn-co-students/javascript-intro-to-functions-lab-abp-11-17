3243191a8fe9e66ef8bb4ee29333e2546527be98
Ryan Lee <ryanlee91@gmail.com>
2017-11-11 14:10:46 +0000

    Done.

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   var lowercase = "hello!"
   lowercase.toLowerCase() === lowercase
-  return string = 'I can\'t hear you!'
+  return 'I can\'t hear you!'
 }
 function sayHiToGrandma(string){
   var uppercase = "HELLO!"
   uppercase.toUpperCase() === uppercase
-  return string = 'YES INDEED!'
+  return 'YES INDEED!'
 }
 
 function sayHiToGrandma(string) {
   var mixedCase = 'I love you, Grandma.'
   mixedCase.toLowerCase() === mixedCase // false
   mixedCase.toUpperCase() === mixedCase // false
-  return string = 'I love you, too.'
+  return 'I love you, too.'
 }
