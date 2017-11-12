86e83e8ed5e45798e793cb410ebe4acb8ee0e34a
marls628 <marlyshawley@outlook.com>
2017-11-12 18:49:32 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,20 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log()
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  string.toLowerCase()
+  if (string === string.toLowerCase()){
+  return "I can't hear you!";
+  }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
