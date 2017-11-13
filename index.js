af8660e17a6b25ca326fe1a325893acd8cbcb757
Cory West-Forbes <cwestforbes@gmail.com>
2017-11-13 19:03:40 +0000

    Done.

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function SayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase) {
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
   }
 }
