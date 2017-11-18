b6385ba74afeb83b3efebce53dc2fbabd088bc41
Dynisha Faust <dgeekgirl08@gmail.com>
2017-11-18 17:55:03 +0000

    Done.

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ uppercase.toUpperCase()== uppercase
 let mixed = "I love you, Grandma";
 
 function sayHiToGrandma(string){
-  if(string.toLowerCase()){
+  if(string.toLowerCase()=== string){
   return "I can't hear you!"
  }
- else if(string.toUpperCase()) {
+ else if(string.toUpperCase()=== string) {
    return "YES INDEED!"
  }
-else {
+else if(string=== "I love you, Grandma.") {
   return 'I love you, too.'
 }
  }
