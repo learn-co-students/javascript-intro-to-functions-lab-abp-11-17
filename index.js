180965865adbdc193613d7c9002ed84453bab0b2
Chely Ho <adept_inept@yahoo.com>
2017-11-21 08:25:53 +0000

    Done.

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   switch(true){
-    case string === string.toLowerCase():
+    case string === whisper(string):                    //used === string.toLowerCase() originally. this is better
       return "I can't hear you!";
-    case string === string.toUpperCase():
+    case string === shout(string):                      // same as above
       return "YES INDEED!";
     case string === "I love you, Grandma.":
       return "I love you, too.";
     default:
-      return console.log("You were ignored. The silent treatment. Or she seriously didn't notice you...");
+      return "You were ignored. The silent treatment. Or she seriously didn't notice you...";
   }
 }
