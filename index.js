b01f3424bdaf90a1df44076a35254fa7ebf27c5b
Stephanie D <slr14404@yahoo.com>
2017-11-21 02:18:35 +0000

    Done.

--- index.js
+++ index.js
@@ -15,11 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string); {
-    return "I can't hear you!";
-  } else if (string.toUpperCase() === string); {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!";
-  } else if (string === "I love you Grandma."){
+  }
+  else if (string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
