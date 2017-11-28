026e22440858e39f63855ac4ce7bb41c7fa43e6b
Stephanie D <slr14404@yahoo.com>
2017-11-28 00:01:05 +0000

    Done.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
-  console.log (string.toLowerCase())
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
   else if (string.toUpperCase() === string) {
-    return "YES INDEED!";
+      return "YES INDEED!"
   }
   else if (string === "I love you, Grandma."){
     return "I love you, too.";
