f0637bc05ea32e00fe366f36b50a890e696d79e4
rbarretto <richard.a.barretto@gmail.com>
2017-12-04 00:15:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+}
+function shout(string) {
+  return string
+}
+'Hello!'.toUpperCase() // 'HELLO!'
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+}
+function whisper(string) {
+  return string
+}
+'Hello'.toLowerCase () //'Hello'
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+
+}
+function logShout(string){
+  console.log(string)
+
+}
+'Hello'.toUpperCase ()
+function logShout(string){
+  console.log string.toUpperCase()
+}
